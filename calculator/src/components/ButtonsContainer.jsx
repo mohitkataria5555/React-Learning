b1d8df90b314a3dcdedf28f22c0842fb6983ffd6
mohitkataria5555 <mohitkataria5555@gmail.com>
2024-03-19 14:19:50 +0530

    clac

--- calculator/src/components/ButtonsContainer.jsx
+++ calculator/src/components/ButtonsContainer.jsx
@@ -1,13 +1,31 @@
-
-import styles from './Buttons.module.css'
+import styles from "./Buttons.module.css";
 const ButtonContainer = () => {
-    const btns = ['C','1','2','+','3','4','-','5','6','*','7','8','/','=','9','0','.']
-    return <div className={styles.buttonsContainer}>
-
-        {btns.map((btn) =>(<button className={styles.button}>C</button>))}
-
-  </div>
-
-}
+  const btns = [
+    "C",
+    "1",
+    "2",
+    "+",
+    "3",
+    "4",
+    "-",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "/",
+    "=",
+    "9",
+    "0",
+    ".",
+  ];
+  return (
+    <div className={styles.buttonsContainer}>
+      {btns.map((btn) => (
+        <button className={styles.button}>C</button>
+      ))}
+    </div>
+  );
+};
 
 export default ButtonContainer;
