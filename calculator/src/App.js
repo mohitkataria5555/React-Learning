82aa87f81f27e2953735f02a2c867511040ebaa1
mohitkataria5555 <mohitkataria5555@gmail.com>
2024-03-19 14:19:13 +0530

    Calculator 1st version

---
+++ calculator/src/App.js
@@ -0,0 +1,15 @@
+
+import styles from "./App.module.css"
+import ButtonContainer from "./components/ButtonsContainer";
+import Display from "./components/Display";
+
+function App() {
+  return (
+    <div className={styles.calculator}>
+      <Display/>
+      <ButtonContainer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ calculator/src/components/ButtonsContainer.jsx
@@ -0,0 +1,13 @@
+
+import styles from './Buttons.module.css'
+const ButtonContainer = () => {
+    const btns = ['C','1','2','+','3','4','-','5','6','*','7','8','/','=','9','0','.']
+    return <div className={styles.buttonsContainer}>
+
+        {btns.map((btn) =>(<button className={styles.button}>C</button>))}
+
+  </div>
+
+}
+
+export default ButtonContainer;
